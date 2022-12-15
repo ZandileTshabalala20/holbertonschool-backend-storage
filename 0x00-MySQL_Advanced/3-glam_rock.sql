4eb8f756df1564ca482e6c02c1aaaa7a0fc7eb62
Zandile Tshabalala <zTshabalala@SINGULAR.CO.ZA>
2022-12-15 09:21:45 +0200

    commit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
--- Lists all bands with Glam rock AS their main style, ranked by their longevity
+-- Search bands with style Glam rock
+-- Durantion current
 
-SELECT  band_name,(ifnull(split,2020) - ifnull(formed,0)) lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC 
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%';
